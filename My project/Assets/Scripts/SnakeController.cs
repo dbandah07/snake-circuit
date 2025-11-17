46380c4c1dae3233e8e6d41a928efb73088af1dc
Daniela Banda-Hernandez <dbh7202006@gmail.com>
2025-11-17 01:04:34 -0800

    draft

---
+++ My project/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    // store direction 
+    // move snake forward
+    // store body seg pos
+    // add seg when collecting packets
+    // detect collisions
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
